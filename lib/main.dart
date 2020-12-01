9c70d2810101943fe312e13d52f3bf28bd1956f5
Rene Lazo <stark@aleph.engineering>
2020-11-30 21:48:26 -0500

    Refactor main UI

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ML Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter ML Demo'),
     );
   }
 }
@@ -36,11 +36,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List _output;
+  var _output;
   File _image;
   var picker = ImagePicker();
   List<CameraDescription> cameras;
-  CameraController controller;
+  CameraController cameraCtrl;
   bool takePhoto;
 
   @override
@@ -48,60 +48,204 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     takePhoto = false;
 
-//    loadModel().then((value) {
-//      setState(() {});
-//    });
-
     loadModel();
     initializeCameras();
   }
 
   loadModel() async {
     await Tflite.loadModel(
-        model: 'assets/dl_cnn/model_unquant.tflite',
-        labels: 'assets/dl_cnn/labels.txt');
+      model: 'assets/dl_cnn/model_unquant.tflite',
+      labels: 'assets/dl_cnn/labels.txt',
+//      isAsset: false,
+    );
   }
 
   Future<void> initializeCameras() async {
     cameras = await availableCameras();
-    initializeController();
+
+    print('availableCameras');
+    print(cameras);
+    await initializeController();
+    print('Camera ${cameras[1]} initialized');
   }
 
-  initializeController() {
-    controller = CameraController(cameras[0], ResolutionPreset.medium);
-    controller.initialize().then((_) {
-      if (!mounted) return;
-      if (takePhoto) {
-        const interval = const Duration(seconds: 3);
-        new Timer.periodic(interval, (Timer t) => capturePictures());
-      }
-    });
+  initializeController() async {
+    cameraCtrl = CameraController(cameras[1], ResolutionPreset.medium);
+
+    try {
+      await cameraCtrl.initialize();
+    } on CameraException catch (e) {
+      print('CameraException $e');
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
   }
 
-  capturePictures() async {
-    var timestamp = DateTime.now().millisecondsSinceEpoch.toString();
-    final Directory extDir = await getApplicationDocumentsDirectory();
-    final String dirPath = "${extDir.path}/Pictures/flutter_test";
-    await Directory(dirPath).create(recursive: true);
-    final String filePath = "$dirPath/($timestamp).png";
-    takePhoto = true;
-
-    if (takePhoto) {
-      controller.takePicture(filePath).then((_) async {
-        if (takePhoto) {
-          File imgFile = File(filePath);
-          var res = await classifyImage(imgFile);
-          print('Class detected:');
-          print(res);
-          takePhoto = false;
-        } else {
-          return;
-        }
+  _clear() {
+    if(mounted) {
+      setState(() {
+        _image = null;
+        _output = null;
       });
     }
   }
 
+  Widget _buildCameraPreviewWidget() {
+    if (cameraCtrl == null || !cameraCtrl.value.isInitialized) {
+      return CircularProgressIndicator();
+    } else {
+      return AspectRatio(
+        aspectRatio: cameraCtrl.value.aspectRatio,
+        child: CameraPreview(cameraCtrl),
+      );
+    }
+  }
+
+  _buildThumbnailImg() {
+    List<Widget> list = new List();
+
+    if(cameras == null) {
+      return Container();
+    }
+
+    if(_image == null) {
+      list.add(
+        new Icon(Icons.photo, size: 64, color: Colors.black26,)
+      );
+    } else {
+      list.addAll([
+        SizedBox(
+          child: Image.file(_image),
+          width: 64.0,
+          height: 64.0,
+        ),
+        new IconButton(icon: Icon(Icons.delete), onPressed: _clear),
+      ]);
+    }
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: list,
+      ),
+    );
+  }
+
+  /// Display a row of toggle to select the camera (or a message if no camera is available).
+  Widget _cameraTogglesRowWidget() {
+    final List<Widget> toggles = <Widget>[];
+
+    if (cameras == null) {
+      return const Text('Initializing camera controller...');
+    }
+    if (cameras.isEmpty) {
+      return const Text('No camera found');
+    } else {
+      for (CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          SizedBox(
+            width: 90.0,
+            child: RadioListTile<CameraDescription>(
+              title: Icon(getCameraLensIcon(cameraDescription.lensDirection)),
+              groupValue: cameraCtrl?.description,
+              value: cameraDescription,
+              onChanged: cameraCtrl != null && cameraCtrl.value.isRecordingVideo
+                  ? null
+                  : onNewCameraSelected,
+            ),
+          ),
+        );
+      }
+    }
+
+    return Row(children: toggles, mainAxisAlignment: MainAxisAlignment.center,);
+  }
+
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (cameraCtrl != null) {
+      await cameraCtrl.dispose();
+    }
+    cameraCtrl = CameraController(
+      cameraDescription,
+      ResolutionPreset.medium,
+      enableAudio: false,
+    );
+
+    // If the controller is updated then update the UI.
+    cameraCtrl.addListener(() {
+      if (mounted) setState(() {});
+      if (cameraCtrl.value.hasError) {
+        print('Camera error ${cameraCtrl.value.errorDescription}');
+      }
+    });
+
+    try {
+      await cameraCtrl.initialize();
+    } on CameraException catch (e) {
+      print('Camera error $e');
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  /// Returns a suitable camera icon for [direction].
+  IconData getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+    }
+    throw ArgumentError('Unknown lens direction');
+  }
+
+  capturePictures() async {
+    print('capturePictures()');
+
+    if (cameraCtrl.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    var timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = "${extDir.path}/Pictures/flutter_test";
+    await Directory(dirPath).create(recursive: true);
+    final String filePath = "$dirPath/($timestamp).jpg";
+
+    print('capturePictures() - filePath: "$filePath"');
+
+    try {
+      await cameraCtrl.takePicture(filePath);
+    } on CameraException catch (e) {
+      print('CameraException - Error: $e');
+      return null;
+    }
+
+    _image = File(filePath);
+    await classifyImage(_image);
+  }
+
+  pickImage() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+
+    print('Start classification');
+    await classifyImage(_image);
+  }
+
   Future<List<dynamic>> classifyImage(File image) async {
+    print('classifyImage()');
+
     var output = await Tflite.runModelOnImage(
         path: image.path,
         numResults: 2,
@@ -111,22 +255,49 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _output = output;
     });
+
+    print('classifyImage() - output');
     return output;
   }
 
-  pickImage() async {
-    var image = await picker.getImage(source: ImageSource.camera);
-    if (image == null) return null;
+  String getClassifyLabel(result) {
+    print('getClassifyLabel()');
+    print('getClassifyLabel() - result: $result');
 
-    setState(() {
-      _image = File(image.path);
-    });
+    String label = result['label'].toString();
+    double confidence = result['confidence'];
+    String confidenceStr = confidence.toStringAsFixed(2);
 
-    print('Start classification');
-    var res = await classifyImage(_image);
+    int idx = label.indexOf(' ');
+    return label.substring(idx).trim() + ' ($confidenceStr)';
+  }
 
-    print('Class detected:');
-    print(res);
+  _buildClassifyLabelWdt() {
+    List resultList = _output;
+    List<Widget> widgetsList = new List();
+
+    if(_image != null) {
+      if(resultList == null || resultList.isEmpty) {
+        widgetsList.add(
+            new Chip(label: new Text('UNKNOWN'),)
+        );
+      } else {
+        for(var res in resultList) {
+          String label = getClassifyLabel(res);
+          widgetsList.add(
+              new Chip(label: new Text(label))
+          );
+        }
+      }
+    } else {
+      widgetsList.add(
+          new Container()
+      );
+    }
+
+    return Row(
+      children: widgetsList,
+    );
   }
 
 
@@ -135,20 +306,34 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            tooltip: 'Import from Gallery',
+            icon: Icon(Icons.add_photo_alternate),
+            onPressed: pickImage,
+          )
+        ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildThumbnailImg(),
+                _buildClassifyLabelWdt(),
+                _buildCameraPreviewWidget(),
+                _cameraTogglesRowWidget(),
+              ],
             ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: capturePictures,
-        tooltip: 'Increment',
+        tooltip: 'Capture from Camera',
         child: Icon(Icons.camera_alt),
       ),
     );
